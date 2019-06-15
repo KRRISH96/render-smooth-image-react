2a069118fb9fe84dfffd5c2c652b1e05854f1dbc
sai <kskprasad96@gmail.com>
2019-06-15 21:33:07 +0530

    image smooth loading implemented

---
+++ src/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import './styles.scss';
+
+class RenderSmoothImage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageLoaded: false,
+      isValidSrc: !!props.src,
+    };
+  }
+
+  onImageLoad = () => {
+    this.setState({
+      imageLoaded: true,
+    });
+  };
+
+  handleImageLoadingFail = () => {
+    this.setState({
+      isValidSrc: false,
+    });
+  };
+
+  render() {
+    const { imageLoaded, isValidSrc } = this.state;
+    const { src, alt = 'not found' } = this.props;
+
+    return (
+      <div className="smooth-image-wrapper">
+        {isValidSrc ? (
+          <img
+            className={`smooth-image img-${imageLoaded ? 'visible' : 'hidden'}`}
+            src={src}
+            alt={alt}
+            onLoad={this.onImageLoad}
+            onError={this.handleImageLoadingFail}
+          />
+        ) : (
+          <div className="smooth-no-image">
+            {alt}
+          </div>
+        )}
+        {isValidSrc && !imageLoaded && (
+          <div className="smooth-preloader">
+            <div
+              className="spinner"
+              style={{ width: 30, display: 'inline-block' }}
+            />
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default RenderSmoothImage;
